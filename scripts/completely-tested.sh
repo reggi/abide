3bb904b3cc289134cc9ed861fa821a55e1d444d5
Thomas Reggi <thomas@reggi.com>
2018-04-12 03:01:34 -0400

    pkg suite

--- scripts/completely-tested.sh
+++ scripts/completely-tested.sh
@@ -13,22 +13,22 @@ results -c -- npm --prefix ./packages/journey.fn-reduce run test
 results -c -- npm --prefix ./packages/journey.is-promise run test
 results -c -- npm --prefix ./packages/journey.pass-thru run test
 # echo 'Testing "pkg" suite...'
-# # results -c -- npm --prefix ./packages/pkg run test
-# # results -c -- npm --prefix ./packages/pkg-cli run test
-# results -c -- npm --prefix ./packages/pkg-plugin-babel-6-to-node-4 run test
-# results -c -- npm --prefix ./packages/pkg-plugin-babel-7-to-node-4 run test
-# results -c -- npm --prefix ./packages/pkg-plugin-cobalt run test
-# results -c -- npm --prefix ./packages/pkg-plugin-jest run test
-# results -c -- npm --prefix ./packages/pkg-plugin-name-dir run test
-# results -c -- npm --prefix ./packages/pkg-plugin-name-scope run test
-# results -c -- npm --prefix ./packages/pkg-plugin-prop run test
-# results -c -- npm --prefix ./packages/pkg-plugin-sort run test
-# results -c -- npm --prefix ./packages/pkg-plugin-standard run test
-# results -c -- npm --prefix ./packages/pkg.file-exists run test
-# results -c -- npm --prefix ./packages/pkg.fs run test
-# results -c -- npm --prefix ./packages/pkg.is-local-module run test
-# results -c -- npm --prefix ./packages/pkg.json-parse run test
-# results -c -- npm --prefix ./packages/pkg.pretty-json run test
-# results -c -- npm --prefix ./packages/pkg.prop-overwrite run test
-# results -c -- npm --prefix ./packages/pkg.read-json run test
-# results -c -- npm --prefix ./packages/pkg.throw-error run test
+# results -c -- npm --prefix ./packages/pkg run test
+# results -c -- npm --prefix ./packages/pkg-cli run test
+results -c -- npm --prefix ./packages/pkg-plugin-babel-6-to-node-4 run test
+results -c -- npm --prefix ./packages/pkg-plugin-babel-7-to-node-4 run test
+results -c -- npm --prefix ./packages/pkg-plugin-cobalt run test
+results -c -- npm --prefix ./packages/pkg-plugin-jest run test
+results -c -- npm --prefix ./packages/pkg-plugin-name-dir run test
+results -c -- npm --prefix ./packages/pkg-plugin-name-scope run test
+results -c -- npm --prefix ./packages/pkg-plugin-prop run test
+results -c -- npm --prefix ./packages/pkg-plugin-sort run test
+results -c -- npm --prefix ./packages/pkg-plugin-standard run test
+results -c -- npm --prefix ./packages/pkg.file-exists run test
+results -c -- npm --prefix ./packages/pkg.fs run test
+results -c -- npm --prefix ./packages/pkg.is-local-module run test
+results -c -- npm --prefix ./packages/pkg.json-parse run test
+results -c -- npm --prefix ./packages/pkg.pretty-json run test
+results -c -- npm --prefix ./packages/pkg.prop-overwrite run test
+results -c -- npm --prefix ./packages/pkg.read-json run test
+results -c -- npm --prefix ./packages/pkg.throw-error run test
