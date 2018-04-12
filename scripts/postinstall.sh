39e2d080315e788884ce939f7b3fc8f0524c00dc
Thomas Reggi <thomas@reggi.com>
2018-04-12 02:46:24 -0400

    pre and post install scripts

---
+++ scripts/postinstall.sh
@@ -0,0 +1,1 @@
+./node_modules/.bin/lerna exec -- ../../node_modules/.bin/dep-merge ./ --unmerge
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,3 @@
 npm i lerna
 npm i ./packages/dep-merge-cli
 ./node_modules/.bin/lerna exec -- ../../node_modules/.bin/dep-merge ./
-npm i
-./node_modules/.bin/lerna exec -- ../../node_modules/.bin/dep-merge ./ --unmerge
