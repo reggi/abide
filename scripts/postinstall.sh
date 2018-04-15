43fcfd5c3cd74bb976598e7cd8ed63c11c96abda
Thomas Reggi <thomas@reggi.com>
2018-04-15 06:45:28 -0400

    proper shebang

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source ./scripts/ensure-installed.sh "dep-merge" "./packages/dep-merge-cli"
 source ./scripts/ensure-installed.sh "lerna" "lerna"
 echo 'post-install: dep-merge unmerging'
