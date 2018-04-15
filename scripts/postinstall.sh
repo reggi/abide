6b2bc7dac7bb4931ea55e1fcfb56dc8dd78e364c
Thomas Reggi <thomas@reggi.com>
2018-04-15 07:03:54 -0400

    better messaging

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-source ./scripts/abspath.sh
-source ./scripts/ensure-installed.sh "dep-merge" "./packages/dep-merge-cli"
-source ./scripts/ensure-installed.sh "lerna" "lerna"
-echo 'post-install: dep-merge unmerging'
-dep_merge_path=`which $dep_merge`
-dep_merge_abs=`abspath $dep_merge_path`
-echo $dep_merge_path
-echo $dep_merge_abs
+source ./scripts/reqinstall.sh
+echo 'post-install: dep-merge unmerging...'
 $lerna exec -- $dep_merge_abs ./ --unmerge
+echo 'post-install: dep-merge done'
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-source ./scripts/abspath.sh
-source ./scripts/ensure-installed.sh "dep-merge" "./packages/dep-merge-cli"
-source ./scripts/ensure-installed.sh "lerna" "lerna"
-echo 'post-install: dep-merge merging'
-dep_merge_path=`which $dep_merge`
-dep_merge_abs=`abspath $dep_merge_path`
-echo $dep_merge_path
-echo $dep_merge_abs
+source ./scripts/reqinstall.sh
+echo 'pre-install: dep-merge merging...'
 $lerna exec -- $dep_merge_abs ./
+echo 'pre-install: dep-merge done'
