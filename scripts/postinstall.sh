90421ec3677410af26f6c073eb004ca5158475e4
Thomas Reggi <thomas@reggi.com>
2018-04-15 06:58:25 -0400

    abspath" ;

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+source ./scripts/abspath.sh
 source ./scripts/ensure-installed.sh "dep-merge" "./packages/dep-merge-cli"
 source ./scripts/ensure-installed.sh "lerna" "lerna"
 echo 'post-install: dep-merge unmerging'
-$lerna exec -- $dep_merge ./ --unmerge
+dep_merge_path=`which $dep_merge`
+dep_merge_abs=`abspath $dep_merge_path`
+echo $dep_merge_path
+echo $dep_merge_abs
+$lerna exec -- $dep_merge_abs ./ --unmerge
