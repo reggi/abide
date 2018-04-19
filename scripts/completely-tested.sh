5619fa33bc0e3574cb1b1f8e4b7c71a4b6b9c118
Thomas Reggi <thomas@reggi.com>
2018-04-19 01:54:15 -0400

    added pkg to completley tested

--- scripts/completely-tested.sh
+++ scripts/completely-tested.sh
@@ -14,7 +14,7 @@ results -c -- npm --prefix ./packages/journey.fn-reduce run test
 results -c -- npm --prefix ./packages/journey.is-promise run test
 results -c -- npm --prefix ./packages/journey.pass-thru run test
 echo 'Testing "pkg" suite...'
-# results -c -- npm --prefix ./packages/pkg run test
+results -c -- npm --prefix ./packages/pkg run test
 # results -c -- npm --prefix ./packages/pkg-cli run test
 results -c -- npm --prefix ./packages/pkg-plugin-babel-6-to-node-4 run test
 results -c -- npm --prefix ./packages/pkg-plugin-babel-7-to-node-4 run test
