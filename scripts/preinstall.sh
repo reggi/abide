09ff416661e30b6f1a28ad8f2c1eddd20b463054
Thomas Reggi <socialtr@gmail.com>
2018-04-20 12:37:45 -0400

    Update preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./scripts/reqinstall.sh
 echo 'pre-install: dep-merge merging...'
-$lerna exec -- $dep_merge_abs ./
+$lerna exec -- $dep_merge_abs --merge --path ./
 echo 'pre-install: dep-merge done'
