83988e88559aef3783ac21e84cb78427926556ce
Thomas Reggi <thomas@reggi.com>
2018-04-21 01:00:20 -0400

    loose-ends

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./scripts/reqinstall.sh
 echo 'post-install: dep-merge unmerging...'
-$lerna exec -- $dep_merge_abs --unmerge --merge --path ./
+$lerna exec -- $dep_merge_abs --unmerge --path ./
 echo 'post-install: dep-merge done'
