af327d8178f110eba8b12625600d2e0c62aeb6e8
Thomas Reggi <thomas@reggi.com>
2018-06-21 00:37:51 -0400

    series javascript

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'collectCoverage': true,
+  'coveragePathIgnorePatterns': [
+    '<rootDir>/unpublished/.+',
+    '<rootDir>/packages/.+/examples/.+',
+    '.+build.js',
+    '<rootDir>packages/pkg-plugin-prop/index.build.js'
+  ],
+  'modulePathIgnorePatterns': [
+    '<rootDir>/unpublished/.+',
+    '<rootDir>/packages/.+/examples/.+',
+    '.+build.js'
+  ],
+  'testPathIgnorePatterns': [
+    '<rootDir>/unpublished/.+',
+    '<rootDir>/packages/.+/examples/.+',
+    '.+build.js'
+  ],
+  'watchPathIgnorePatterns': [
+    '<rootDir>/unpublished/.+',
+    '<rootDir>/packages/.+/examples/.+',
+    '.+build.js'
+  ]
+}
