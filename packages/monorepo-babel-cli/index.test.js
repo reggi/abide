1f44dd41207066a20cd681070781f792e252c79d
Thomas Reggi <thomas@reggi.com>
2018-06-22 03:07:03 -0400

    meowcakes 3

--- packages/monorepo-babel-cli/index.test.js
+++ packages/monorepo-babel-cli/index.test.js
@@ -2,142 +2,142 @@ import mockFs from 'mock-fs'
 import sinon from 'sinon'
 import monorepoBabelCli from './index'
 
-test('meowcakes', () => {
-  expect(true).toBe(true)
+const getArgs = (argv, workingDir) => ({
+  argv,
+  stderr: {write: sinon.spy()},
+  stdout: {write: sinon.spy()},
+  exit: sinon.spy(),
+  cwd: () => workingDir
+})
+const dependencies = {'lodash': '4.0.0'}
+const devDependencies = {'sinon': '4.0.0'}
+const babelA = {'plugins': [ 'transform-object-rest-spread' ]}
+const babelB = {'plugins': [ 'transform-runtime' ]}
+const babelC = {'plugins': [ 'pumpkin-soup' ]}
+
+const babelValidRoot = {'plugins': [
+  ...babelA.plugins,
+  ...babelB.plugins,
+  ...babelC.plugins
+]}
+
+const babelInvalidRoot = {'plugins': [
+  ...babelA.plugins,
+  ...babelB.plugins
+]}
+
+beforeEach(async () => {
+  mockFs({
+    '/working-case': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
+    },
+    '/non-working-case': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'fs-extra': '4.0.0'}})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
+    },
+    '/non-working-case-multiple-versions-same-dev-dep': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.2'}})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.3'}})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
+    },
+    '/non-working-case-multiple-versions-same-dep': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies: {'sinon': '4.0.1'}})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies: {'sinon': '4.0.2'}})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies: {'sinon': '4.0.3'}})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
+    },
+    '/non-working-case-invalid-root-version': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.1'}})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.1'}})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
+    },
+    '/non-working-case-missing-root': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.1'}})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.1'}})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {}})
+    },
+    '/working-case-babel': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies, babel: babelValidRoot})
+    },
+    '/non-working-case-babel-missing-root': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
+    },
+    '/non-working-case-babel': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies, babel: babelInvalidRoot})
+    },
+    '/working-case-with-package-missing-packagejson': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})},
+        'd': {}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
+    },
+    '/working-case-with-packages-missing-dev-or-deps': {
+      'packages': {
+        'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies})},
+        'b': {'package.json': JSON.stringify({name: '@reggi/example-b', devDependencies})},
+        'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
+      },
+      'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
+      'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
+    }
+  })
 })
 
-// const getArgs = (argv, workingDir) => ({
-//   argv,
-//   stderr: {write: sinon.spy()},
-//   stdout: {write: sinon.spy()},
-//   exit: sinon.spy(),
-//   cwd: () => workingDir
-// })
-// const dependencies = {'lodash': '4.0.0'}
-// const devDependencies = {'sinon': '4.0.0'}
-// const babelA = {'plugins': [ 'transform-object-rest-spread' ]}
-// const babelB = {'plugins': [ 'transform-runtime' ]}
-// const babelC = {'plugins': [ 'pumpkin-soup' ]}
+afterEach(async () => {
+  mockFs.restore()
+})
 
-// const babelValidRoot = {'plugins': [
-//   ...babelA.plugins,
-//   ...babelB.plugins,
-//   ...babelC.plugins
-// ]}
-
-// const babelInvalidRoot = {'plugins': [
-//   ...babelA.plugins,
-//   ...babelB.plugins
-// ]}
-
-// beforeEach(async () => {
-//   mockFs({
-//     '/working-case': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
-//     },
-//     '/non-working-case': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'fs-extra': '4.0.0'}})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
-//     },
-//     '/non-working-case-multiple-versions-same-dev-dep': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.2'}})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.3'}})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
-//     },
-//     '/non-working-case-multiple-versions-same-dep': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies: {'sinon': '4.0.1'}})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies: {'sinon': '4.0.2'}})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies: {'sinon': '4.0.3'}})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
-//     },
-//     '/non-working-case-invalid-root-version': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.1'}})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.1'}})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {'sinon': '4.0.3'}})
-//     },
-//     '/non-working-case-missing-root': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies: {'sinon': '4.0.1'}})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies: {'sinon': '4.0.1'}})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies: {'sinon': '4.0.1'}})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies: {}})
-//     },
-//     '/working-case-babel': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies, babel: babelValidRoot})
-//     },
-//     '/non-working-case-babel-missing-root': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
-//     },
-//     '/non-working-case-babel': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies, babel: babelA})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies, babel: babelB})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies, babel: babelC})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies, babel: babelInvalidRoot})
-//     },
-//     '/working-case-with-package-missing-packagejson': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies, devDependencies})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', dependencies, devDependencies})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})},
-//         'd': {}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
-//     },
-//     '/working-case-with-packages-missing-dev-or-deps': {
-//       'packages': {
-//         'a': {'package.json': JSON.stringify({name: '@reggi/example-a', dependencies})},
-//         'b': {'package.json': JSON.stringify({name: '@reggi/example-b', devDependencies})},
-//         'c': {'package.json': JSON.stringify({name: '@reggi/example-c', dependencies, devDependencies})}
-//       },
-//       'lerna.json': JSON.stringify({'packages': ['./packages/*']}),
-//       'package.json': JSON.stringify({'name': 'root', dependencies, devDependencies})
-//     }
-//   })
-// })
-
-// afterEach(async () => {
-//   mockFs.restore()
-// })
+test('meowcakes 3', () => {
+  expect(true).toBe(true)
+})
 
 // test('monorepo-babel-cli: help', async () => {
 //   const args = getArgs(['node', './index', '--help'])
