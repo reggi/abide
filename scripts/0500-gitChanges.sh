58848d43d4173b8901ad0be61d2b649be38dbf15
Thomas Reggi <thomas@reggi.com>
2018-06-23 14:36:36 -0400

    diff

--- scripts/0500-gitChanges.sh
+++ scripts/0500-gitChanges.sh
@@ -1,6 +1,7 @@
 USAGE="--gitChanges (throws error code if there are git changes)"
 function gitChanges () {
   git status --porcelain
+  git diff
   CHANGED=$(git status --porcelain)
   if [ -n "${CHANGED}" ]; then
     exit 1
