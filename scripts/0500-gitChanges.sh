11cb725150cc504f332aad904488013b74fe29f7
Thomas Reggi <thomas@reggi.com>
2018-06-23 14:36:00 -0400

    sed linix with-i

--- scripts/0500-gitChanges.sh
+++ scripts/0500-gitChanges.sh
@@ -1,14 +1,11 @@
 USAGE="--gitChanges (throws error code if there are git changes)"
 function gitChanges () {
-  stderrBanner "gitChanges" "checking if there is changes in the current dir"
   git status --porcelain
-  # CHANGED=$(git status --porcelain)
-  # if [ -n "${CHANGED}" ]; then
-  #   stderrBanner "gitChanges" "found changed files"
-  #   exit 1
-  # else
-  #   stderrBanner "gitChanges" "no changed files"
-  #   exit 0
-  # fi
+  CHANGED=$(git status --porcelain)
+  if [ -n "${CHANGED}" ]; then
+    exit 1
+  else
+    exit 0
+  fi
 }
 
