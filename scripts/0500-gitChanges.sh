dd9345c367b58770df78253620cb9cf9283dbcd4
Thomas Reggi <thomas@reggi.com>
2018-06-23 13:47:13 -0400

    git status --porcelain

--- scripts/0500-gitChanges.sh
+++ scripts/0500-gitChanges.sh
@@ -1,13 +1,14 @@
 USAGE="--gitChanges (throws error code if there are git changes)"
 function gitChanges () {
   stderrBanner "gitChanges" "checking if there is changes in the current dir"
-  CHANGED=$(git status --porcelain)
-  if [ -n "${CHANGED}" ]; then
-    stderrBanner "gitChanges" "found changed files"
-    exit 1
-  else
-    stderrBanner "gitChanges" "no changed files"
-    exit 0
-  fi
+  git status --porcelain
+  # CHANGED=$(git status --porcelain)
+  # if [ -n "${CHANGED}" ]; then
+  #   stderrBanner "gitChanges" "found changed files"
+  #   exit 1
+  # else
+  #   stderrBanner "gitChanges" "no changed files"
+  #   exit 0
+  # fi
 }
 
